bd6c0133ede4003948f6aa31cbed55606b29004e
Maria <pam_735@yahoo.es>
2017-07-22 13:58:04 +0200

    Footer

--- js/main.js
+++ js/main.js
@@ -26,4 +26,19 @@ $(document).ready(function () {
     var prev = $(this).prev();
     prev.removeClass('logoActiveHover logoHover');
   });
+
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
 });
