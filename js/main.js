efb21a47477b17afd20ee3d54ce55fcb0fe9c5c6
Maria <pam_735@yahoo.es>
2017-07-26 21:23:12 +0200

    AFD

--- js/main.js
+++ js/main.js
@@ -27,6 +27,13 @@ $(document).ready(function () {
     prev.removeClass('logoActiveHover logoHover');
   });
 
+  var url = window.location;
+  $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+
+  $('ul.nav a').filter(function() {
+    return this.href == url;
+  }).parent().addClass('active');
+
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
     if (this.hash !== "") {
